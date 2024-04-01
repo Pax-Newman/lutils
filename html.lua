ee28df99ef67a026dff3bfb08111e0f994092765
Pax-Newman <newmanp@wwu.edu>
2024-03-31 19:08:05 -0700

    html dsl now prerenders static fragments and escapes inputs

--- html.lua
+++ html.lua
@@ -1,42 +1,114 @@
----- An experiment on meta programming and backend html generation
+---- An experiment on DSLs and backend html generation
 
+---Options for creating a new element
 ---@alias opts { singleton? : boolean }
+---Partially rendered html piece
+---@alias partial (string|fun(opts: table<string, any>): string)[]
+---A piece of dynamic rendering code in an html partial
+---@alias codepiece fun(opts?: table<string, any>): string
+
+local function join(arr1, arr2)
+   for i = 1, #arr2 do
+      arr1[#arr1 + 1] = arr2[i]
+   end
+end
+
+---Appends a piece of an html partial to a partial
+---@param arr partial
+---@param val string | codepiece | partial
+local function appendChunk(arr, val)
+   -- If the new element and last element are strings, attach them
+   if type(val) == "string" and type(arr[#arr]) == "string" then
+      arr[#arr] = arr[#arr] .. val
+   elseif type(val) == "table" then
+      for i = 1, #val do
+         appendChunk(arr, val[i])
+      end
+   else
+      table.insert(arr, val)
+   end
+end
 
 --- Element Factory
 ---@param tag string
 ---@param opts? opts
----@return fun(data: table): string
+---@return fun(data: table<number|string, string|codepiece|partial>): partial
 local function Element(tag, opts)
    opts = opts or {}
 
    return function(data)
-      local attributes = ""
+      local attributes = {}
       local contents = {}
 
       for index, value in pairs(data) do
          if type(index) == "string" then
-            attributes = attributes .. string.format(' %s="%s"', index:gsub("_", "-"), value)
+            -- If the key is a string, then it's an attribute
+            -- attributes = attributes .. string.format(' %s="%s"', index:gsub("_", "-"), value)
+            appendChunk(attributes, string.format(' %s="', index:gsub("_", "-")))
+            appendChunk(attributes, value)
+            appendChunk(attributes, '"')
          elseif type(index) == "number" then
-            table.insert(contents, value)
+            -- If the key is a number, then it's a child element
+            appendChunk(contents, value)
          end
       end
 
-      -- local attr_str = table.concat(attributes, " ")
-      local opening = string.format("<%s%s>", tag, attributes)
-
-      if opts.singleton then
-         return opening
+      -- Add opening tag
+      if type(attributes[1]) == "string" then
+         attributes[1] = "<" .. tag .. attributes[1]
+         appendChunk(attributes, ">")
+      elseif #attributes == 0 and type(contents[1]) == "string" then
+         contents[1] = "<" .. tag .. ">" .. contents[1]
+      else
+         table.insert(attributes, 1, "<" .. tag)
+         appendChunk(attributes, ">")
       end
 
-      local closing = string.format("</%s>", tag)
-      return opening .. table.concat(contents, "") .. closing
+      join(attributes, contents)
+
+      if not opts.singleton then
+         appendChunk(attributes, string.format("</%s>", tag))
+      end
+
+      return attributes
    end
 end
 
-local html = {
-   div = Element("div"),
-   a = Element("a"),
-   img = Element("img", { singleton = true }),
-}
+---Renders an HTML chunk
+---@param partial partial
+---@param writer fun(str: string) Handles the render html output, will be called multiple times as the rendering occurs. If left blank, render will return a string of the rendered html
+---@param opts? table<string, any>
+---@return nil | string
+local function render(partial, writer, opts)
+   local out = nil
+   if not writer then
+      out = ""
+      writer = function(str)
+         out = out .. str
+      end
+   end
+   opts = opts or {}
 
-return html
+   -- Escape any strings in the input opts
+   for key, value in pairs(opts) do
+      if type(value) == "string" then
+         opts[key] = value:gsub('"', "&quot;"):gsub("<", "&lt;"):gsub(">", "&gt;")
+      end
+   end
+
+   -- Render and gather html fragments
+   for _, item in ipairs(partial) do
+      if type(item) == "string" then
+         writer(item)
+      elseif type(item) == "function" then
+         writer(item(opts))
+      end
+   end
+
+   return out
+end
+
+return {
+   Element = Element,
+   render = render,
+}
