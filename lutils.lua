a5f5769627e83939aa777af46ec48ac955ed932d
Pax-Newman <newmanp@wwu.edu>
2024-04-02 21:36:25 -0700

    moved each submodule to it's own dir

---
+++ lutils.lua
@@ -0,0 +1,10 @@
+---- Imports sub-modules
+
+package.path = package.path .. ";?/?.lua"
+
+return {
+   html = require("html"),
+   enumerable = require("enumerable"),
+   command = require("command"),
+   fenv = require("fenv"),
+}
