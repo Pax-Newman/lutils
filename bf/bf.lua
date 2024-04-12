97e390155877af286459d103c57d2363d3e7ad1a
Pax-Newman <newmanp@wwu.edu>
2024-04-11 22:50:42 -0700

    pretty printer is prettier, parser represents tokens as ints internally

--- bf/bf.lua
+++ bf/bf.lua
@@ -3,6 +3,7 @@
 ---- Parser
 
 ---@class Parser
+---@field opmap table<integer, string>
 ---@field source string[]
 ---@field cur string
 ---@field peek string
@@ -12,17 +13,25 @@ local Parser = {}
 function Parser:New(source)
    local obj = {}
 
-   -- PERF: Storings chars as an int may be more space and time efficient
-   local chars = {}
-   for char in source:gmatch(".") do
-      table.insert(chars, char)
-   end
+   obj.opmap = {
+      [62] = ">",
+      [60] = "<",
+      [43] = "+",
+      [45] = "-",
+      [46] = ".",
+      [44] = ",",
+   }
 
-   obj.source = chars
+   obj.loopmap = {
+      [91] = "[",
+      [93] = "]",
+   }
+
+   obj.source = { string.byte(source, 1, #source) }
    obj.ptr = 1
 
-   obj.cur = chars[1]
-   obj.peek = chars[2]
+   obj.cur = obj.source[1]
+   obj.peek = obj.source[2]
 
    obj.program = {}
 
@@ -45,7 +54,7 @@ end
 function Parser:Parse()
    while self.cur ~= nil do
       -- Check for a loop
-      if self.cur == "[" then
+      if self.cur == 91 then -- [
          local loop, err = self:parseLoop()
          -- Stop if we encountered an error while parsing the loop
          if err then
@@ -53,13 +62,14 @@ function Parser:Parse()
          end
 
          table.insert(self.program, loop)
-      elseif self.cur == "]" then
+      elseif self.cur == 93 then -- ]
          -- We shouldn't ever see a closing bracket outside of parseLoop
          return nil, self:error("Unexpected ] encountered")
       end
 
       -- Ingest valid non-loop tokens
-      if string.match("><+-.,", self.cur) ~= nil then
+
+      if self.opmap[self.cur] ~= nil then
          table.insert(self.program, self.cur)
       end
 
@@ -80,19 +90,19 @@ function Parser:parseLoop()
 
    while self.cur ~= nil do
       -- Recursively parse if we see a new loop
-      if self.cur == "[" then
+      if self.cur == 91 then -- [
          local inner, err = self:parseLoop()
          if err then
             return nil, err
          end
          table.insert(loop, inner)
       -- Stop when we see a closing bracket
-      elseif self.cur == "]" then
+      elseif self.cur == 93 then -- ]
          return loop, nil
       end
 
       -- Until then continue ingesting code
-      if string.match("><+-.,", self.cur) ~= nil then
+      if self.opmap[self.cur] ~= nil then
          table.insert(loop, self.cur)
       end
       self:next()
@@ -104,6 +114,9 @@ end
 function Parser:printInner(program, depth)
    depth = depth or 0
 
+   local out = ""
+   local space = true
+
    local indent = ""
    for _ = 0, depth do
       indent = indent .. "  "
@@ -111,20 +124,58 @@ function Parser:printInner(program, depth)
 
    for _, node in ipairs(program) do
       if type(node) == "table" then
-         print(indent .. "[")
-         self:printInner(node, depth + 1)
-         print(indent .. "]")
+         space = true
+         out = out .. "\n" .. indent .. "["
+         out = out .. "\n" .. self:printInner(node, depth + 1)
+         out = out .. "\n" .. indent .. "]" .. "\n"
       else
-         print(indent .. node)
+         if space then
+            out = out .. indent
+         end
+         out = out .. self.opmap[node]
+         space = false
       end
    end
+
+   return out
 end
 
 function Parser:PrettyPrint()
-   self:printInner(self.program, 0)
+   print(self:printInner(self.program, 0))
 end
 
-local prog = "><+_.hey there this is a comment lol,-[[+]]-"
+-- local prog = "--+><[--[++]]><"
+
+local prog = [[
+-,+[                         Read first character and start outer character reading loop
+    -[                       Skip forward if character is 0
+        >>++++[>++++++++<-]  Set up divisor (32) for division loop
+                               (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
+        <+<-[                Set up dividend (x minus 1) and enter division loop
+            >+>+>-[>>>]      Increase copy and remainder / reduce divisor / Normal case: skip forward
+            <[[>+<-]>>+>]    Special case: move remainder back to divisor and increase quotient
+            <<<<<-           Decrement dividend
+        ]                    End division loop
+    ]>>>[-]+                 End skip loop; zero former divisor and reuse space for a flag
+    >--[-[<->+++[-]\]\]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
+        ++++++++++++<[       If flag then set up divisor (13) for second division loop
+                               (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
+            >-[>+>>]         Reduce divisor; Normal case: increase remainder
+            >[+[<+>-]>+>>]   Special case: increase remainder / move it back to divisor / increase quotient
+            <<<<<-           Decrease dividend
+        ]                    End division loop
+        >>[<+>-]             Add remainder back to divisor to get a useful 13
+        >[                   Skip forward if quotient was 0
+            -[               Decrement quotient and skip forward if quotient was 1
+                -<<[-]>>     Zero quotient and divisor if quotient was 2
+            ]<<[<<->>-]>>    Zero divisor and subtract 13 from copy if quotient was 1
+        ]<<[<<+>>-]          Zero divisor and add 13 to copy if quotient was 0
+    ]                        End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
+    <[-]                     Clear remainder from first division if second division was skipped
+    <.[-]                    Output ROT13ed character from copy and clear it
+    <-,+                     Read next character
+]
+]]
 
 local test_parser = Parser:New(prog)
 local test_program, err = test_parser:Parse()
