29d52a11ff304ac42f21c52d6a37fbf32d02c2f8
Pax-Newman <newmanp@wwu.edu>
2024-04-12 12:32:42 -0700

    removed test code, added pretty printer for vm, added simple repl

--- bf/bf.lua
+++ bf/bf.lua
@@ -163,49 +163,6 @@ function Parser:PrettyPrint()
    print(self:printInner(self.program, 0))
 end
 
--- local prog = "--+><[--[++]]><"
-
-local prog = [[
--,+[                         Read first character and start outer character reading loop
-    -[                       Skip forward if character is 0
-        >>++++[>++++++++<-]  Set up divisor (32) for division loop
-                               (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
-        <+<-[                Set up dividend (x minus 1) and enter division loop
-            >+>+>-[>>>]      Increase copy and remainder / reduce divisor / Normal case: skip forward
-            <[[>+<-]>>+>]    Special case: move remainder back to divisor and increase quotient
-            <<<<<-           Decrement dividend
-        ]                    End division loop
-    ]>>>[-]+                 End skip loop; zero former divisor and reuse space for a flag
-    >--[-[<->+++[-]\]\]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
-        ++++++++++++<[       If flag then set up divisor (13) for second division loop
-                               (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
-            >-[>+>>]         Reduce divisor; Normal case: increase remainder
-            >[+[<+>-]>+>>]   Special case: increase remainder / move it back to divisor / increase quotient
-            <<<<<-           Decrease dividend
-        ]                    End division loop
-        >>[<+>-]             Add remainder back to divisor to get a useful 13
-        >[                   Skip forward if quotient was 0
-            -[               Decrement quotient and skip forward if quotient was 1
-                -<<[-]>>     Zero quotient and divisor if quotient was 2
-            ]<<[<<->>-]>>    Zero divisor and subtract 13 from copy if quotient was 1
-        ]<<[<<+>>-]          Zero divisor and add 13 to copy if quotient was 0
-    ]                        End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
-    <[-]                     Clear remainder from first division if second division was skipped
-    <.[-]                    Output ROT13ed character from copy and clear it
-    <-,+                     Read next character
-]
-]]
-
-local test_parser = Parser:New(prog)
-local test_program, err = test_parser:Parse()
-
-if err then
-   print(err)
-   os.exit(1)
-end
-
-test_parser:PrettyPrint()
-
 ---- Virtual Machine
 
 ---@class Machine
@@ -297,6 +254,46 @@ function Machine:Eval(program, loop)
    end
 end
 
-local test_vm = Machine:New()
+function Machine:PrettyPrint()
+   local fmt = [[
 
-test_vm:Eval(test_program)
+|-------|-------|-------|
+| %05d | %05d | %05d |
+|-------|-------|-------|
+|  %03d  |  %03d  |  %03d  |
+|---<---|---|---|--->---|
+]]
+   --stylua: ignore
+   local out = fmt:format(
+      self.cell - 1,
+      self.cell,
+      self.cell + 1,
+      self.state[self.cell - 1] or 0,
+      self.state[self.cell],
+      self.state[self.cell + 1] or 0
+   )
+   print(out)
+end
+
+local function REPL()
+   local vm = Machine:New()
+   vm:PrettyPrint()
+
+   local input = ""
+   while input ~= nil do
+      io.write("> "):flush()
+      input = io.read()
+
+      local parser = Parser:New(input)
+      local program, err = parser:Parse()
+
+      if err then
+         print(err)
+      else
+         vm:Eval(program)
+         vm:PrettyPrint()
+      end
+   end
+end
+
+REPL()
