8739dd0505d752d3fd4b1b3b7268d81e08a4b2a3
Pax-Newman <newmanp@wwu.edu>
2024-04-17 20:56:38 -0700

    added additional ansi formatters, cleaned code up a bit

--- colorful/colorful.lua
+++ colorful/colorful.lua
@@ -1,7 +1,20 @@
-local function hex(num)
-   return tonumber(num, 16)
+---- ANSI Text Styling
+
+local function parseHex(num)
+   local r = num:sub(2, 3)
+   local g = num:sub(4, 5)
+   local b = num:sub(6, 7)
+
+   assert(#r == 2 and #g == 2 and #b == 2, "Improperly formatted color")
+
+   return tonumber(r, 16), tonumber(g, 16), tonumber(b, 16)
 end
 
+---@alias item { [1] : string, fg : string, bg : string, under : boolean, strike : boolean, over : boolean, bold : boolean, italic : boolean, blink : boolean, fastblink : boolean}
+
+---Applies ANSI text formatting
+---@param opts item | item[]
+---@return string
 local function style(opts)
    if type(opts[1]) == "string" then
       opts = { opts }
@@ -16,26 +29,42 @@ local function style(opts)
 
       ---- Apply foreground color
       if item.fg then
-         -- \033[38;2;<r>;<g>;<b>m     #Select RGB foreground color
-         local fg = item.fg
-
-         local r = fg:sub(2, 3)
-         local g = fg:sub(4, 5)
-         local b = fg:sub(6, 7)
-         assert(#r == 2 and #g == 2 and #b == 2, "Improperly formatted color")
-         table.insert(effects, string.format("38;2;%d;%d;%d", hex(r), hex(g), hex(b)))
+         -- \033[38;2;<r>;<g>;<b>m     Select RGB foreground color
+         table.insert(effects, string.format("38;2;%d;%d;%d", parseHex(item.fg)))
       end
 
       ---- Apply background color
       if item.bg then
-         -- \033[48;2;<r>;<g>;<b>m     #Select RGB background color
-         local bg = item.bg
+         -- \033[48;2;<r>;<g>;<b>m     Select RGB background color
+         table.insert(effects, string.format("48;2;%d;%d;%d", parseHex(item.bg)))
+      end
 
-         local r = bg:sub(2, 3)
-         local g = bg:sub(4, 5)
-         local b = bg:sub(6, 7)
-         assert(#r == 2 and #g == 2 and #b == 2, "Improperly formatted color")
-         table.insert(effects, string.format("48;2;%d;%d;%d", hex(r), hex(g), hex(b)))
+      if item.bold then
+         table.insert(effects, 1)
+      end
+
+      if item.italic then
+         table.insert(effects, 3)
+      end
+
+      if item.under then
+         table.insert(effects, 4)
+      end
+
+      if item.blink then
+         table.insert(effects, 5)
+      end
+
+      if item.fastblink then
+         table.insert(effects, 6)
+      end
+
+      if item.strike then
+         table.insert(effects, 9)
+      end
+
+      if item.over then
+         table.insert(effects, 53)
       end
 
       local eff_str = table.concat(effects, ";")
