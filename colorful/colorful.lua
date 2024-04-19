6760182755ffd43af77f1c7f8c72ef4bbd8bd21a
Pax-Newman <newmanp@wwu.edu>
2024-04-18 20:57:09 -0700

    added some luadocs and refactored hex color int parsing

--- colorful/colorful.lua
+++ colorful/colorful.lua
@@ -1,5 +1,10 @@
 ---- ANSI Text Styling
 
+---Converts a hex color string (e.g. #808080) to individual base10 values for rgb channels
+---@param num string
+---@return integer
+---@return integer
+---@return integer
 local function parseHex(num)
    local r = num:sub(2, 3)
    local g = num:sub(4, 5)
@@ -10,6 +15,21 @@ local function parseHex(num)
    return tonumber(r, 16), tonumber(g, 16), tonumber(b, 16)
 end
 
+---Splits a hex color integer (e.g. 0x808080) into its respective rgb channels
+---@param color integer
+---@return integer
+---@return integer
+---@return integer
+local function splitHex(color)
+   assert(0x000000 <= color and color <= 0xffffff, string.format("Color #%x out of bounds", color))
+
+   local r = (color & 0xff0000) >> 16
+   local g = (color & 0x00ff00) >> 8
+   local b = (color & 0x0000ff)
+   return r, g, b
+end
+
+---The start of any escape sequence
 local ESC = string.char(27, 91)
 
 local function apply(codes, obj)
@@ -17,6 +37,7 @@ local function apply(codes, obj)
    return obj
 end
 
+--- Contains functions to apply an ANSI format escape sequence to text
 local fmtMap = {
    bold = function(obj)
       return apply({ 1 }, obj)
@@ -41,14 +62,23 @@ local fmtMap = {
    end,
 }
 
-local style = {}
-function style:new(str)
+---@class FString
+---@field val string Internal string representation
+---@field bg (fun(color: string | integer): FString) | table<string|integer, integer[]>
+local FString = {}
+
+---Wrap a string using the FString object
+---@param str string
+---@return FString
+function FString:new(str)
    local obj = {}
 
    setmetatable(obj, self)
    self.__index = function(_, idx)
       if idx == "val" then
          return str
+      elseif idx == "isFstring" then
+         return true
       end
 
       return fmtMap[idx](obj)
@@ -57,14 +87,14 @@ function style:new(str)
    return obj
 end
 
-function style:__tostring()
+function FString:__tostring()
    -- PERF: Right now we're repeating a lot of opening and closing escapes
    -- but we could instead track each code and apply them efficiently at render-time here
    return self.val
 end
 
 local colors = {}
-function style.SetColor(name, hex)
+function FString.SetColor(name, hex)
    colors[name] = { parseHex(hex) }
 end
 
@@ -74,10 +104,7 @@ fmtMap.fg = function(obj)
          local r, g, b = table.unpack(colors[color])
          return apply({ 38, 2, r, g, b }, obj)
       elseif type(color) == "number" then
-         local r = (color & 0xff0000) >> 16
-         local g = (color & 0x00ff00) >> 8
-         local b = (color & 0x0000ff)
-         return apply({ 38, 2, r, g, b }, obj)
+         return apply({ 38, 2, splitHex(color) }, obj)
       end
    end
    return setmetatable({}, {
@@ -92,10 +119,7 @@ fmtMap.bg = function(obj)
          local r, g, b = table.unpack(colors[color])
          return apply({ 48, 2, r, g, b }, obj)
       elseif type(color) == "number" then
-         local r = (color & 0xff0000) >> 16
-         local g = (color & 0x00ff00) >> 8
-         local b = (color & 0x0000ff)
-         return apply({ 48, 2, r, g, b }, obj)
+         return apply({ 48, 2, splitHex(color) }, obj)
       end
    end
    return setmetatable({}, {
@@ -104,4 +128,4 @@ fmtMap.bg = function(obj)
    })
 end
 
-return style
+return FString
