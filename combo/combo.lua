cad21fdc35b4ca788851692bf6688b2d9d1267ed
Pax-Newman <newmanp@wwu.edu>
2024-05-26 21:23:32 -0700

    combo: combo now operates on arrays rather than strings to be more flexible

--- combo/combo.lua
+++ combo/combo.lua
@@ -1,6 +1,126 @@
 -- I wanted to learn about parser combinators so I'm trying to build one
 
-return {
-   ---Contains functions for parsing strings into other strings
-   string2string = require "string2string",
-}
+local utils = require "utils"
+
+---@generic T
+---@class ComboInput
+---@field next fun(): `T`
+
+---@alias Token { type: string, literal: string }
+---@alias Result { success: boolean, value: any[], rest: string, captures: table}
+---@alias ComboPiece fun(val: Array): Result
+
+local exports = {}
+
+---Matches one of the target
+---@param target any Must implement __eq
+---@return ComboPiece
+function exports.one(target)
+   return function(val)
+      if val[1] ~= nil and target == val[1] then
+         return {
+            success = true,
+            value = { val[1] },
+            rest = { table.unpack(val, 2) },
+            captures = {},
+         }
+      end
+
+      return { success = false }
+   end
+end
+
+---Returns the result of the first combo that successfully matches
+---@param ... ComboPiece
+---@return ComboPiece
+function exports.any(...)
+   local combos = table.pack(...)
+
+   return function(val)
+      local res
+
+      for _, combo in ipairs(combos) do
+         res = combo(val)
+         if res.success then
+            return res
+         end
+      end
+
+      return { success = false }
+   end
+end
+
+---Returns successfully even if the wrapped combo doesn't match
+---@param combo ComboPiece
+---@return ComboPiece
+function exports.optional(combo)
+   return function(val)
+      local res = combo(val)
+
+      if res.success then
+         return res
+      end
+
+      return { success = true, value = val, rest = {}, captures = {} }
+   end
+end
+
+---Succeeds if it can match the combo at least n times
+---@param n integer
+---@param combo ComboPiece
+---@return ComboPiece
+function exports.atLeast(n, combo)
+   return function(val)
+      local res = { success = false, value = {}, captures = {} }
+
+      local step = combo(val)
+
+      res.success = step.success
+
+      while step.success do
+         res.value = utils.concatArrays(res.value, step.value)
+         res.rest = step.rest
+         res.captures = utils.merge(res.captures, step.captures)
+         step = combo(step.rest)
+      end
+
+      if not res.success or #res.value < n then
+         return { success = false }
+      else
+         return res
+      end
+   end
+end
+
+---Matches an exact sequence of combos
+---@param ... ComboPiece
+---@return ComboPiece
+function exports.sequence(...)
+   local combos = table.pack(...)
+
+   return function(val)
+      local res = { success = true, rest = val }
+
+      for _, combo in ipairs(combos) do
+         local step = combo(res.rest)
+
+         if not step.success then
+            return { success = false }
+         end
+
+         res.value = utils.concatArrays(res.value or {}, step.value)
+         res.rest = step.rest
+         res.captures = utils.merge(res.captures or {}, step.captures)
+      end
+
+      return res
+   end
+end
+
+function exports.wrapStr(f)
+   return function(str)
+      return f(utils.strToArr(str))
+   end
+end
+
+return exports
--- combo/utils.lua
+++ combo/utils.lua
@@ -18,6 +18,10 @@ function exports.merge(t1, t2)
    return t1
 end
 
+function exports.concatArrays(t1, t2)
+   return table.move(t2, 1, #t2, #t1 + 1, t1)
+end
+
 function exports.prettytable(t)
    for key, value in pairs(t) do
       if type(value) == "table" then
@@ -28,6 +32,17 @@ function exports.prettytable(t)
          print(string.format("%s: %s", key, value))
       end
    end
+end
+
+---Turns a string into an array of characters
+---@param str string
+---@return string[]
+function exports.strToArr(str)
+   local t = {}
+   for c in str:gmatch "." do
+      table.insert(t, c)
+   end
+   return t
 end
 
 return exports
